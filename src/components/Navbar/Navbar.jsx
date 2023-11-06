8302f83068c76ab81a7854a9c1e2f487abe33a96
rocktohq <itzmonir@gmail.com>
2023-11-06 07:51:33 +0600

    Navbar Updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,72 +1,63 @@
-import { Avatar, Dropdown, Navbar } from "flowbite-react";
+import { Avatar, Button, Dropdown, Navbar } from "flowbite-react";
 import { Link, NavLink } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
 
 function Nav() {
+  const { user, signOutUser } = useAuth();
+  const handleLogout = () => {
+    signOutUser();
+  };
   return (
     <Navbar fluid rounded>
-      <Navbar.Brand>
-        <Link to="/" className="flex items-center">
-          <img src="/favicon.png" className="mr-3 h-6 sm:h-9" alt="RideRelay" />
-          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-            RideRelay
-          </span>
-        </Link>
-      </Navbar.Brand>
+      <Link to="/" className="flex items-center">
+        <img src="/favicon.png" className="mr-3 h-6 sm:h-9" alt="RideRelay" />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+          RideRelay
+        </span>
+      </Link>
       <div className="flex md:order-2">
-        <Dropdown
-          arrowIcon={true}
-          inline
-          label={
-            <Avatar
-              alt="User settings"
-              img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-              rounded
-            />
-          }
-        >
-          <Dropdown.Header>
-            <span className="block text-sm">Saidul Mursalin</span>
-            <span className="block truncate text-sm font-medium">
-              monir@gmail.com
-            </span>
-          </Dropdown.Header>
-          <Dropdown.Item>Dashboard</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item>Sign Out</Dropdown.Item>
-        </Dropdown>
+        {user?.email ? (
+          <Dropdown
+            arrowIcon={true}
+            inline
+            label={
+              <Avatar
+                alt="User settings"
+                img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+                rounded
+              />
+            }
+          >
+            <Dropdown.Header>
+              <span className="block text-sm">{user?.displayName}</span>
+              <span className="block truncate text-sm font-medium">
+                {user?.email}
+              </span>
+            </Dropdown.Header>
+            <Dropdown.Item>Dashboard</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item onClick={handleLogout}>Sign Out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to="/login">
+            <Button color="purple" className="rounded">
+              Login
+            </Button>
+          </Link>
+        )}
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link>
-          <NavLink
-            to="/"
-            className={({ isActive, isPending }) =>
-              isPending ? "pending" : isActive ? "active" : ""
-            }
-          >
-            Home
-          </NavLink>
-        </Navbar.Link>
-        <Navbar.Link>
-          <NavLink
-            to="/services"
-            className={({ isActive, isPending }) =>
-              isPending ? "pending" : isActive ? "active" : ""
-            }
-          >
-            Services
-          </NavLink>
-        </Navbar.Link>
-        <Navbar.Link>
-          <NavLink
-            to="/login"
-            className={({ isActive, isPending }) =>
-              isPending ? "pending" : isActive ? "active" : ""
-            }
-          >
-            Login
-          </NavLink>
-        </Navbar.Link>
+        <NavLink to="/" className="text-lg pb-1 border-b lg:border-none">
+          Home
+        </NavLink>
+
+        <NavLink
+          to="/services"
+          className="text-lg pb-1 border-b lg:border-none"
+        >
+          Services
+        </NavLink>
       </Navbar.Collapse>
     </Navbar>
   );
