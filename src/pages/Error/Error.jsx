44f71acf7339cfc3f505c73abed6d1aa51667fc7
rocktohq <itzmonir@gmail.com>
2023-11-08 00:05:20 +0600

    Error Page Updated

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,5 +1,34 @@
+import Lottie from "lottie-react";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import notFound from "../../assets/animations/404.json";
+import Header from "../../components/Header/Header";
+import Foot from "../../components/Footer/Footer";
+import { Button } from "flowbite-react";
+import { Link } from "react-router-dom";
+import { AiFillHome } from "react-icons/ai";
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <HelmetProvider>
+      <Helmet>
+        <title>404</title>
+      </Helmet>
+      <Header />
+      <div className="flex flex-col justify-center items-center">
+        <div className="h-3/4">
+          <Lottie animationData={notFound} loop={false} />
+        </div>
+        <div className="">
+          <Link to="/">
+            <Button color="purple" className="rounded">
+              <AiFillHome />
+              &nbsp;Go Back to Home
+            </Button>
+          </Link>
+        </div>
+      </div>
+      <Foot />
+    </HelmetProvider>
+  );
 };
 
 export default Error;
